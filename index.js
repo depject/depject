898ab3b5d84f47769357602c2085ecf4b6ab70b6
Dominic Tarr <dominic.tarr@gmail.com>
2016-12-03 17:20:38 +0100

    merge in load order, so that cycles are possible

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function isEmpty (e) {
   return true
 }
 
+function isFunction (f) {
+  return 'function' === typeof f
+}
+
 var isArray = Array.isArray
 
 var apply = require('./apply')
@@ -46,21 +50,50 @@ module.exports  = function combine () {
     return a
   }, {})
 
+  var allNeeds = {}, allGives = {}
+  for (var k in modules) {
+    var module = modules[k]
+    N.each(module.needs, function (v, path) {
+      N.set(allNeeds, path, true)
+    })
+    if(isString(module.gives))
+      N.set(allGives, [module.gives], true)
+    else
+      N.each(module.gives, function (v, path) {
+        N.set(allGives, path, true)
+      })
+  }
+
+  N.each(allNeeds, function (_, path) {
+    if(!N.get(allGives, path))
+      console.log('MISSING', path)
+  })
+
+  console.log("NEEDS", allNeeds)
+  console.log("GIVES", allGives)
+
+
+  //okay, instead of iterating over everything, in dependency order.
+  //just create things in order added?
+
   var sockets = {}
   while (true) {
     var newSockets = {}
     //filter modules that have not been resolved yet.
-    modules = filter(modules, function (module) {
+    modules = filter(modules, function (module, key) {
       //if this module cannot be resolved yet, keep it for next time.
-      if(!N.each(module.needs, function (type, path) {
-        if(!N.get(sockets, path)) return false
-      })) return true
-
+//      if(!N.each(module.needs, function (type, path) {
+//        if(!N.get(sockets, path)) return false
+//      })) return true
+//
       //collect the functions that this module needs.
       var m = N.map(module.needs, function (type, path) {
           var a = N.get(sockets, path)
-          if(isArray(a)) return apply[type](a)
-          else           throw new Error('expected array at path:'+path.join('.'))
+          if(!a)
+            a = N.set(sockets, path, [])
+          return apply[type](a)
+//          if(isArray(a)) return apply[type](a)
+//          else           throw new Error('expected array at path:'+path.join('.'))
         })
 
       //create module, and get function(s) it returns.
@@ -71,12 +104,17 @@ module.exports  = function combine () {
         append(newSockets, [module.gives], exported)
       else
         N.each(module.gives, function (_, path) {
-          append(newSockets, path, N.get(exported, path))
+          var fun = N.get(exported, path)
+          if(!isFunction(fun))
+            throw new Error('export declared but not returned'+path.join('.') + ' in:'+key)
+          append(newSockets, path, fun)
         })
     })
 
-    if(isEmpty(newSockets))
+    if(isEmpty(newSockets)) {
+      console.log(sockets, modules)
       throw new Error('could not resolve all modules')
+    }
     else
       N.each(newSockets, function (_ary, path) {
         var ary = N.get(sockets, path) || N.set(sockets, path, [])
@@ -86,8 +124,6 @@ module.exports  = function combine () {
       return sockets
   }
 }
-
-
 
 
 
