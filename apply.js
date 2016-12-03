db0dc78786aee3eda60fe8761932fd504eff3fac
Dominic Tarr <dominic.tarr@gmail.com>
2016-12-03 17:20:06 +0100

    accept more than one argument

--- apply.js
+++ apply.js
@@ -7,19 +7,26 @@ module.exports = {
     }
   },
   first: function (funs) {
+    if(funs.length) throw new Error('depject.map: plugins not loaded yet')
     return function (value) {
+      var args = [].slice.call(arguments)
       for(var i = 0; i < funs.length; i++) {
-        var _value = funs[i](value)
+        var _value = funs[i].apply(this, args)
         if(_value) return _value
       }
     }
   },
   map: function (funs) {
+    if(funs.length) throw new Error('depject.map: plugins not loaded yet')
     return function (value) {
+      var args = [].slice.call(arguments)
       return funs.map(function (fn) {
-        return fn(value)
+        return fn.apply(this, args)
       })
     }
   }
 }
+
+
+
 
