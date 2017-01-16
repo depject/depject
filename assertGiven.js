f7f9d2772b4135fbfa7381ff7fb09f7eaeec89e6
Michael Williams <michael.williams@enspiral.com>
2017-01-17 00:10:37 +1300

    implemented and test nested assertGives

--- assertGiven.js
+++ assertGiven.js
@@ -1,3 +1,5 @@
+var N = require('libnested')
+
 module.exports = function assertGiven (gives, given) {
   if (!given) {
     throw new Error('create function should return a function or an object')
@@ -6,19 +8,18 @@ module.exports = function assertGiven (gives, given) {
   if (typeof gives === 'string' && typeof given !== 'function') {
     throw new Error('create function should return a function when gives is a string')
   } else if (isObject(gives) && isObject(given)) {
-    if (!hasSameKeys(gives, given)) {
-      throw new Error('keys returned by create must match keys in given')
-    }
+    firstMissingKey(gives, given, function (path) {
+      throw new Error('keys returned by create must match keys in given. missing: ' + path.join(', '))
+    })
   }
 }
 
-function hasSameKeys (gives, given) {
-  return hasEveryKey(gives, given) && hasEveryKey(given, gives)
-}
-
-function hasEveryKey (obj1, obj2) {
-  return Object.keys(obj1).every(function (key) {
-    return obj2[key]
+function firstMissingKey (gives, given, onMissingKey) {
+  return N.each(gives, function (value, path) {
+    if (N.get(given, path) === undefined) {
+      onMissingKey(path)
+      return false
+    }
   })
 }
 
