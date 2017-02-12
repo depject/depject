27ecafdc1a16d7dbd41a53eb58f06770f70c8536
Michael Williams <michael.williams@enspiral.com>
2017-02-13 00:13:30 +1300

    export `getNeeded` as `depject/entry`
    change arguments to `(sockets, needs) => api`
    
    use case is so [this](https://github.com/ssbc/patchbay/blob/822d7aba3e5e907a98c48a9abe3223cc278c6c95/index.js#L9)
    
    ```js
    var sockets = combine(modules)
    sockets.app[0]()
    ```
    
    can instead be
    
    ```js
    var sockets = combine(modules)
    var api = entry(sockets, { app: 'first' })
    api.app()
    ```
    
    this (maybe) fixes the design question
    
    > Should `combine` have a way to specify the public interface?

---
+++ entry.js
@@ -0,0 +1,13 @@
+var N = require('libnested')
+
+var apply = require('./apply')
+
+module.exports = function entry (sockets, needs) {
+  return N.map(needs, function (type, path) {
+    var dependency = N.get(sockets, path)
+    if (!dependency) {
+      dependency = N.set(sockets, path, [])
+    }
+    return apply[type](dependency)
+  })
+}
