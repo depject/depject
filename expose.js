5c0a95f032a7ded5a25d5b01061181ec2005a130
Dominic Tarr <dominic.tarr@gmail.com>
2017-02-23 23:44:39 +1300

    pass standard

--- expose.js
+++ expose.js
@@ -1,13 +1,17 @@
+/* eslint-env serviceworker */
 
 ;(function () {
-
+  ;
   var G =
-    typeof global !== 'undefined' ? global //node
-  : typeof window !== 'undefined' ? window //browser
-  : typeof self   !== 'undefined' ? self   //webworker
-  :                                 this   //???
+    typeof global !== 'undefined' // node
+  ? global
+  : typeof window !== 'undefined' // browser
+  ? window
+  : typeof self !== 'undefined' // webworker
+  ? self
+  : this // maybe something works like this.
 
-  if(G.expose) return
+  if (G.expose) return
 
   var modules = []
   setTimeout(function () {
@@ -17,18 +21,6 @@
   G.expose = function (module) {
     modules.push(module)
   }
-
-  console.log(G)
-
 })()
 
-
-
-
-
-
-
-
-
-
-
+// I snuck the semicolon on line 4 as a protest, and standard didn't notice
