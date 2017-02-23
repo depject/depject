5124acbb8439420bca0b92a99b8009aeae4cab0c
Dominic Tarr <dominic.tarr@gmail.com>
2017-02-23 23:18:46 +1300

    tidy

--- expose.js
+++ expose.js
@@ -7,26 +7,28 @@
   : typeof self   !== 'undefined' ? self   //webworker
   :                                 this   //???
 
-  console.log('depject!')
-
   if(G.expose) return
 
-  var modules = {}
+  var modules = []
   setTimeout(function () {
-    console.log(modules)
     require('./entry')(require('./')(modules), {app: 'first'}).app()
   })
 
   G.expose = function (module) {
-    console.log('EXPOSE', module)
-//    var o = {}
-    modules[Math.random()] = module
-  //  modules.push(o)
+    modules.push(module)
   }
 
-  console.log('G', G.expose, G)
+  console.log(G)
 
 })()
+
+
+
+
+
+
+
+
 
 
 
