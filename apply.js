01ae55666e7423d8e6acd54367af3989c3c54fcc
Michael Williams <michael.williams@enspiral.com>
2017-02-28 13:32:35 +1300

    allow for empty applys

--- apply.js
+++ apply.js
@@ -1,7 +1,6 @@
 module.exports = {
   reduce: function (funs) {
     return function (value, context) {
-      if (!funs.length) throw new Error('depject.reduce: no functions available to reduce')
       return funs.reduce(function (value, fn) {
         return fn(value, context)
       }, value)
@@ -9,7 +8,6 @@ module.exports = {
   },
   first: function (funs) {
     return function (value) {
-      if (!funs.length) throw new Error('depject.first: no functions available to take first')
       var args = [].slice.call(arguments)
       for (var i = 0; i < funs.length; i++) {
         var _value = funs[i].apply(this, args)
@@ -19,7 +17,6 @@ module.exports = {
   },
   map: function (funs) {
     return function (value) {
-      if (!funs.length) throw new Error('depject.map: no functions available to map')
       var args = [].slice.call(arguments)
       return funs.map(function (fn) {
         return fn.apply(this, args)
